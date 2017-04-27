0006f87f1eb68afe73f8c3ac73c70e7f379b9327
Belarbi Moammed Amin <belarbi_mohammed_amin@univ-mosta.dz>
2017-04-27 16:43:40 +0200

    Delete script.sh

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# créer un dossier a partager entre le container et la machine hot
-mkdir /home/$(whoami)/qt-projets
-chmod 777 /home/$(whoami)/qt-projets
-DISPLAY="$DISPLAY"
-#ajouter une commande direct qui permet de lancer le docker qt
-echo "nvidia-docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/$(whoami)/qt-projets:/home/developer/qt-projets qt" > /usr/local/bin/qt-docker
-chmod +x /usr/local/bin/qt-docker

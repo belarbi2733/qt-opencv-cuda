f899959af60b15a749b749dcf90f671878d55bbf
Belarbi Moammed Amin <belarbi_mohammed_amin@univ-mosta.dz>
2017-04-27 17:03:58 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ mkdir /opt/qt-projets
 chmod 777 /opt/qt-projets
 #ajouter une commande direct qui permet de lancer le docker qt
 DISPLAY="$DISPLAY"
-echo "nvidia-docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /opt/qt-projets:/home/developer/qt-projets qt" > /usr/local/bin/qt-docker
+echo "nvidia-docker run -ti --rm -e DISPLAY=\$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /opt/qt-projets:/home/developer/qt-projets qt" > /usr/local/bin/qt-docker
 chmod +x /usr/local/bin/qt-docker
